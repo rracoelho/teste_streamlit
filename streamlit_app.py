9a395b553e5fca2d2d4dbd8dbc66d30769e96f0e
rracoelho <129284042+rracoelho@users.noreply.github.com>
2023-03-29 08:34:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+# Define a interface do usuário
+def main():
+    st.title("Meu primeiro aplicativo Streamlit")
+    st.write("Bem-vindo ao meu aplicativo!")
+    name = st.text_input("Digite o seu nome:")
+    st.write(f"Olá, {name}!")
+
+if __name__ == "__main__":
+    main()
 
